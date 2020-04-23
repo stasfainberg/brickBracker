75e2e4569d052429d939c1a40d8bae4cb300d73b
stasfainberg <stas.fainberg@gmail.com>
2020-04-23 22:09:24 +0300

    gameplay-defineVariables

--- BrickBracker/src/brickBracker/Gameplay.java
+++ BrickBracker/src/brickBracker/Gameplay.java
@@ -1,15 +1,43 @@
 package brickBracker;
 
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Timer;
 
 import javax.swing.JPanel;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener{
 
-
+	private boolean play = false;
+	private int score = 0;
+	
+	private int totalBricks =21;
+	
+	private Timer timer;
+	private int delay = 8;
+	
+	private int playerX = 310;
+	
+	private int ballPosX = 120;
+	private int ballPosY = 350;
+	private int ballXdir = -1;
+	private int ballYdir = -2;
+	
+	public Gameplay () {
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		timer = newTimer(delay, this);
+		timer.start();
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
 	
 	
 	
